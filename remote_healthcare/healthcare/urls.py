cb92f33d058b31b2c8e876ce7d8ba374b7e7ed54
David Njoroge <davidndegwa6869@gmail.com>
2023-08-17 14:01:44 +0300

    updated the urls.py and added two views.
    This means that i will have to add additional urls in the views.py file.

--- remote_healthcare/healthcare/urls.py
+++ remote_healthcare/healthcare/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('doctor_profile/<str:doctor_name>/', views.doctor_profile, name='doctor_profile'),
     path('book_appointment/', views.book_appointment, name='book_appointment'),
     path('submit_appointment/', views.submit_appointment, name='submit_appointment'),
+    path('cancel_appointent/', views.cancel_application, name='cancel-application'),
+    path('postpone_appointment/, views.postpone-appointment, name='postpone-appointment'),
 ]
