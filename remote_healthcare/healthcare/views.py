867992a3ba45277d2bb9ca7a3dc16bf4de5438f3
david Njoroge <davidndegwa6869@gmail.com>
2023-09-04 02:38:03 +0300

    views updated login functions

--- remote_healthcare/healthcare/views.py
+++ remote_healthcare/healthcare/views.py
@@ -5,14 +5,30 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
 
-##added these functions for home and login 
-##addding for register in due time 
+
 def home(request):
     return render(request, "healthcare/home.html")
 
 def login(request):
-    return render(request, "healthcare/login.html")
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            messages.success(request, 'You have successfully logged in.')
+            return redirect('dashboard') 
+        else:
+            
+            messages.error(request, 'Invalid username or password. Please try again.')
+    else:
+        form = AuthenticationForm()
+    return render(request, "healthcare/login.html", {"form": form})
+
+def dashboard(request):
+    return render(request, "healthcare/dashboard.html")
 
 def readmore(request):
     return render(request,"healthcare/readmore.html")
@@ -22,7 +38,10 @@ def register(request):
         form = UserCreationForm(request.POST)  # Pass POST data to form to prevent failing csrf
         if form.is_valid():
             form.save()
-            return redirect("login.html")
+            return redirect("dashboard.html")
     else:
         form = UserCreationForm()
     return render(request, "healthcare/register.html", {"form": form})
+
+def logout(request):
+    messages.sucess(request, "logged out successfully")
