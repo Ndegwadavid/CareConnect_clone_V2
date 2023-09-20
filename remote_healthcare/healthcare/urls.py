256fecf8b3427ee99b4da81af98d96e17eb1dd66
david Njoroge <davidndegwa6869@gmail.com>
2023-09-20 13:15:47 +0300

    fallback SSOs #

--- remote_healthcare/healthcare/urls.py
+++ remote_healthcare/healthcare/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from . import views
 from django.contrib.auth import views as auth_views
 
+## migth overwrite the singles without the accounts
 urlpatterns = [
 
     path('', views.home, name='home'),
@@ -13,6 +14,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-path('accounts/login/', auth_views.LoginView.as_view(template_name='healthcare/sign_in.html'), name='custom_login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='healthcare/sign_in.html'), name='custom_login'), ## what happens after click sign in
+    path('accounts/profile/', views.profile_view, name='profile'), ## where to fall after sso sign in
     path('accounts/', include('allauth.urls')),
 ]
